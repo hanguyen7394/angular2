c6c68fa73796fd28565b6c602f43e2cfd3ee226a
Hoang Tuan <tuan.tha@sutrixsolutions.com>
2017-01-06 18:32:02 +0700

    update css

--- service_api/api_product.php
+++ service_api/api_product.php
@@ -1,4 +1,4 @@
-<?php
+ <?php
     header("Access-Control-Allow-Origin: *");
     header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization, If-Modified-Since, Cache-Control, Pragma");
     

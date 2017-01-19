80d024ffb345d8d7a16b5f96e594f1905eded52e
ha.ntt <ha.ntt@sutrixsolutions.com>
2017-01-19 15:16:53 +0700

    edit api_comment.php

--- service_api/api_product.php
+++ service_api/api_product.php
@@ -3,12 +3,9 @@
 	header("Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept, Authorization, If-Modified-Since, Cache-Control, Pragma");
 	
 	include_once("database.php");
-
 	$json = file_get_contents('php://input');
 	$input = json_decode($json);
-
 	$db = new database();
-
 	if($input->product_cate_id)
 	{
 		$lenh_sql = "SELECT *
@@ -45,15 +42,11 @@
 		$db->setQuery($lenh_sql);
 		$data = $db->loadAllRow();
 	}
-
 	$db->disconect();
 	
-
 	//print_r($data);
-
 	//chuyen du lieu thanh json
 	$chuoi =  json_encode($data, JSON_HEX_TAG | JSON_HEX_APOS | JSON_HEX_QUOT | JSON_HEX_AMP | JSON_UNESCAPED_UNICODE);
-
 	//xuất chuỗi ra màn hình
 	echo $chuoi;
 ?>
